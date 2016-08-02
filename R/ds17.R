bab2bc0cde8dc751c23b2bd4ca5f9b957baea15f
ngay <nathanaelwgay@netscape.net>
2016-08-02 07:50:41 -0400

    Changed title of ds17, and changed the version number patch digit in the description file from 0 to 1.

--- R/ds17.R
+++ R/ds17.R
@@ -1,9 +1,9 @@
-#' Data Set 17: Earthquake Measurements
-#' 
-#' The columns are counts of the number of words spoken in a day by male (M) and 
-#' female (F) subjects in six different sample groups.  Column M1 denotes the 
-#' word counts for males in Sample 1, F1 is the count for females in Sample 1, and 
-#' so on.  
+#' Data Set 17: Word Counts by Males and Females
+#'
+#' The columns are counts of the number of words spoken in a day by male (M) and
+#' female (F) subjects in six different sample groups.  Column M1 denotes the
+#' word counts for males in Sample 1, F1 is the count for females in Sample 1, and
+#' so on.
 #' \itemize{
 #' \item Sample 1: Recruited couples ranging in age from 18 to 29
 #' \item Sample 2: Students recruited in introductory psychology classes, aged 17 to 23
@@ -12,7 +12,7 @@
 #' \item Sample 5: Students recruited in introductory psychology classes, aged 18 to 26
 #' \item Sample 6: Students recruited in introductory psychology classes, aged 17 to 23
 #' }
-#' 
+#'
 #' @format A data frame with 56 rows and 12 variables
 #' @source Triola, M. 2014. Elementary Statistics, 12th ed. Boston, MA: Pearson.
 #' @name ds17
