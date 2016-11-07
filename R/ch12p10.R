ba858cf98c74bc3760d9430e79beed6d16ceacc7
ngay <bnz_family@nathanael-gays-imac.local>
2016-11-07 11:29:50 -0500

    Added data for two-way ANOVA.

---
+++ R/ch12p10.R
@@ -0,0 +1,13 @@
+#' Chapter 12.3 Problem#10: Pulse Rates
+#' 
+#' Pulse rates (bpm) by age category (under or over 30) and sex.  
+#' \itemize{
+#' \item pulse.rate
+#' \item Sex
+#' \item Age (categorical as over or under 30)
+#' }
+#' 
+#' @format A data frame with 40 rows and 3 variables
+#' @source Triola, M. 2014. Elementary Statistics, 12th ed. Boston, MA: Pearson.
+#' @name ch12p10
+NULL
---
+++ R/ch12p6.R
@@ -0,0 +1,13 @@
+#' Chapter 12.3 Problem#6: Chest Deceleration (in g)
+#' 
+#' Chest deceleration by car type and size.  
+#' \itemize{
+#' \item Chest.Deceleration (in grams)
+#' \item Type (Foreign or Domestic)
+#' \item Size (Small, Medium, Large)
+#' }
+#' 
+#' @format A data frame with 18 rows and 3 variables
+#' @source Triola, M. 2014. Elementary Statistics, 12th ed. Boston, MA: Pearson.
+#' @name ch12p6
+NULL
