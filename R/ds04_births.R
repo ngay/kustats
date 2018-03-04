b4cacd6f29ddca378dbeefca3d85ca9aa59c73be
ngay <nathanaelwgay@netscape.net>
2018-03-04 13:56:10 -0500

    Added data set from 13th edition.

---
+++ R/ds04_births.R
@@ -0,0 +1,20 @@
+#' Data Set 4: Births
+#'
+#' Data are from 400 births.  For GENDER 1 = male and 0 = female.
+#' LENGTH_OF_STAY is in days, BIRTH_WEIGHT is in grams, and TOTAL_CHARGES
+#' are in dollars.
+#' \itemize{
+#' \item FACILITY
+#' \item INSURANCE
+#' \item GENDER (1 = Male)
+#' \item LENGTH_OF_STAY (days)
+#' \item ADMITTED
+#' \item DISCHARGED
+#' \item BIRTH_WEIGHT (grams)
+#' \item TOTAL_CHARGES (dollars)
+#' }
+#'
+#' @format A data frame with 400 rows and 8 variables
+#' @source Triola, M. 2018. Elementary Statistics, 13th ed. Boston, MA: Pearson.
+#' @name ds04_births
+NULL
