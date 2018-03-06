36277907e7c7189c0a8f4958ffaa0d28534f5e49
ngay <nathanaelwgay@netscape.net>
2018-03-06 10:37:56 -0500

    Added data set 21 from the 13th edition.

---
+++ R/ds21_earthquakes.R
@@ -0,0 +1,15 @@
+#' Data Set 21: Earthquakes
+#'
+#' Data are from 600 matches pairs magnitude/depth measurements randomly selected
+#' from  10,594 earthquakes recorded in one year from a location in southern
+#' California.  Only earthquakes with a magnitude of at least 1.00 are used.
+#'
+#' \itemize{
+#' \item MAGNITUDE is magnitude measured on the Richter scale
+#' \item DEPTH is depth in km.  The magnitude and depth both describe the source of the earthquake.  The data are from the Southern California Earthquake Data Center.
+#' }
+#'
+#' @format A data frame with 600 rows and 2 variables
+#' @source Triola, M. 2018. Elementary Statistics, 13th ed. Boston, MA: Pearson.
+#' @name ds21_earthquakes
+NULL
