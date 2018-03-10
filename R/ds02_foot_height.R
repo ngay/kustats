45e6b42215c72990c6c70c98d2629d878461df2b
ngay <nathanaelwgay@netscape.net>
2018-03-10 12:10:24 -0500

    Added ds02 from the 13th edition.

---
+++ R/ds02_foot_height.R
@@ -0,0 +1,18 @@
+#' Data Set 2: Foot and Height Measurements
+#'
+#' A data set containing data on 40 foot and height measurements.  Data is from "Estimation of Stature from
+#' Foot Length: Applications in Forensic Science, " by Brenda Rohren, M.A., MFS, LIMHP, LADS, MAC.
+#' Brenda Rohren was a graduate student at Nebraska Wesleyan University when she conducted the
+#' research and wrote the report.
+#' \itemize{
+#' \item SEX is gender of subject
+#' \item AGE is in years
+#' \item FOOT_LENGTH foot length (cm)
+#' \item SHOE_SIZE is reported shoe size
+#' \item HEIGHT is is height (cm) of the subject
+#' }
+#'
+#' @format A data frame with 40 rows and 6 variables
+#' @source Triola, M. 2018. Elementary Statistics, 13th ed. Boston, MA: Pearson.
+#' @name ds02_foot_height
+NULL
