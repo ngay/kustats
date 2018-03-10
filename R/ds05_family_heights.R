ae795451d1fa092e75c14185501968ac3182cea4
ngay <nathanaelwgay@netscape.net>
2018-03-10 12:36:45 -0500

    Added ds05 from the 13th edition.

---
+++ R/ds05_family_heights.R
@@ -0,0 +1,19 @@
+#' Data Set 5: Family Heights
+#'
+#' Height data are from 134 families.  Heights are in inches.  Only families
+#' with at least one child of each gender are included, and only heights
+#' of the first son and first daughter are included.  The data are from
+#' a journalof Francis Galton (1822-1911), who developed the concepts of
+#' standard deviation, regression line, and correlation between two
+#' variables
+#' \itemize{
+#' \item FATHER
+#' \item MOTHER
+#' \item FIRST_SON
+#' \item FIRST_DAUGHTER
+#' }
+#'
+#' @format A data frame with 134 rows and 4 variables
+#' @source Triola, M. 2018. Elementary Statistics, 13th ed. Boston, MA: Pearson.
+#' @name ds05_family_heights
+NULL
