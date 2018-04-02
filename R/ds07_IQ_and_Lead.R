8355cc7e3f765fea3ffccd3dc929e45bdbb4d6ad
ngay <nathanaelwgay@netscape.net>
2018-04-02 18:33:08 -0400

    Updated data for the 13th edition.

---
+++ R/ds07_IQ_and_Lead.R
@@ -0,0 +1,26 @@
+#' Data Set 7: IQ and Lead Exposure
+#'
+#' Data are measured from children in two consecutive years, and the
+#' children were living close to a lead smelter.
+#' Data are from "Neuropsychological Dysfunction in Children with
+#' Chronic Low-Level Lead Absorption, " by P.J. Landrigan, R.H.
+#' Whitworth, R.W. Baloh, N.W. Staehling, W.F. Barthel, and B.F. Rosenblum,
+#' Lancet, Vol. 1, Issue 7909.
+#' \itemize{
+#' \item LEAD is blood lead level group [1=low lead level (blood lead
+#' levels < 40 micograms/100 mL in both years; 2 = medium lead level (blood lead levels >= 40 micrograms/100 mL
+#' in exactly one of the two years); 3 = high lead levels (blood lead levels >= 40 micrograms/100 mL
+#' in both years)].
+#' \item AGE is age in years
+#' \item SEX is sex of subject (1=male,2=female)
+#' \item YEAR1 is blood lead level in first year
+#' \item YEAR2 is blood lead level in second year
+#' \item IQ_VERB is measured verbal IQ score
+#' \item IQ_PERF is measured performance IQ score
+#' \item IQ_FULL is measured full IQ score
+#' }
+#'
+#' @format A data frame with 121 rows and 8 variables
+#' @source Triola, M. 2018. Elementary Statistics, 13th ed. Boston, MA: Pearson.
+#' @name ds07_IQ_and_Lead
+NULL
