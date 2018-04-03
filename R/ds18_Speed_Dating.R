c54dd310d5218e5c55ba4ca618dafebc3e6676c6
ngay <nathanaelwgay@netscape.net>
2018-04-03 14:16:42 -0400

    Updated more data sets for the 13th edition.

---
+++ R/ds18_Speed_Dating.R
@@ -0,0 +1,16 @@
+#' Data Set 18: Speed Dating
+#'
+#' Data are from 199 dates.  Based on replication data from \strong{Data Analysis Using Regression and Multilevel/Hierarchical Models},
+#' by Andrew Gelman and Jennifer Hill, Cambridge University Press.  Data for males uses the corresponding decsriptors are defined for females.
+#' \itemize{
+#' \item DEC_BY_FEM is decision (1 = yes) of female to date again
+#' \item AGE_FEM is age of female
+#' \item LIKE_BY_FEM is "like" rating by female of male (scale of 1-10)
+#' \item ATTRACT_BY_FEM is "attractive" rating by female of male (scale 1-10)
+#' \item ATTRIB_BY_FEM is sum of ratinsg of fove attributes (sincerity, intelligence, fun, ambitious, shared interests) by female of male
+#' }
+#'
+#' @format A data frame with 199 rows and 10 variables
+#' @source Triola, M. 2018. Elementary Statistics, 13th ed. Boston, MA: Pearson.
+#' @name ds18_Speed_Dating
+NULL
