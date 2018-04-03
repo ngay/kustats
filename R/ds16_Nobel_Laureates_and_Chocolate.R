4353613dec15fe0827dc64ae8b706b5e41cadea4
ngay <nathanaelwgay@netscape.net>
2018-04-03 13:53:24 -0400

    Updated more data sets for the 13th edition.

---
+++ R/ds16_Nobel_Laureates_and_Chocolate.R
@@ -0,0 +1,20 @@
+#' Data Set 16: Nobel Laureates and Chocolate
+#'
+#' Data are from 23 countries.  The data are from "The
+#' Real Secret to Genius? Reading Between the Lines, " by
+#' McClintock, Stangle, and Cetinkaya-Rundel, \strong{Chance},
+#' Vol. 27, No. 1; and "Chocolate Consumption, Cognitive Function,
+#' and Nobel Laureates," by Franz Messerli, \strong{New England Journal of Medicine},
+#' Vol. 367, No. 16.
+#' \itemize{
+#' \item COUNTRY
+#' \item CHOCOLATE includes chocolate consumption (kg per capita)
+#' \item NOBEL includes the number of Nobel Laureates (per 10 million people)
+#' \item POPULATION includes population (in millions)
+#' \item INTERNET includes the number of Internet users per 100 people
+#' }
+#'
+#' @format A data frame with 23 rows and 5 variables
+#' @source Triola, M. 2018. Elementary Statistics, 13th ed. Boston, MA: Pearson.
+#' @name ds16_Nobel_Laureates_and_Chocolate
+NULL
---
+++ R/ds17_Course_Evaluations.R
@@ -0,0 +1,18 @@
+#' Data Set 17: Course Evaluations
+#'
+#' Data are from 93 college student course evaluations.  Based on data from
+#' Andrew Gelman and Jennifer Hill, 2007, "Replications Data for Data
+#' Analysis Using Regression Multilevel/Hierarchical Models,"
+#' http://hdl.handle.net/1902.1/10285 .
+#' \itemize{
+#' \item COURSE_EVAL includes the mean course rating
+#' \item PROF_EVAL includes the mean professor rating
+#' \item PROF_AGE includes the professor age in years
+#' \item SIZE includes the number of course evaluations per course
+#' \item PROF_BEAUTY includes the mean beauty rating based on the professor's photo
+#' }
+#'
+#' @format A data frame with 93 rows and 6 variables
+#' @source Triola, M. 2018. Elementary Statistics, 13th ed. Boston, MA: Pearson.
+#' @name ds17_Course_Evaluations
+NULL
